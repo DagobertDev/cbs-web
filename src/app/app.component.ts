0ead218be65fd9aef7b5a212ae2bdfc217bbd00a
David <git@davidmannes.de>
2021-08-27 18:27:02 +0200

    Add bike service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Bike} from "../models/bike"
+import {BikeService} from "../services/bike.service"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  bikes: Bike[] | undefined
+
+  constructor(private bikeService: BikeService) {
+  }
+
+  ngOnInit() {
+    this.getBikes()
+  }
+
+  getBikes() {
+    this.bikeService.get().subscribe(bikes => this.bikes = bikes)
+  }
+
   title = 'Community Bike-Sharing';
 }
---
+++ src/services/bike.service.ts
@@ -0,0 +1,19 @@
+import {HttpClient} from "@angular/common/http"
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs"
+import {environment} from "../environments/environment"
+import {Bike} from "../models/bike"
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BikeService {
+  private bikesUrl = environment.apiUrl + '/bikes';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public get(): Observable<Bike[]> {
+    return this.http.get<Bike[]>(this.bikesUrl)
+  }
+}
