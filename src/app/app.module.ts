2e447a2185b19ade269f4cfbb7c7f8a5c61d78eb
David <git@davidmannes.de>
2021-09-08 10:01:55 +0200

    Add some basic input / button styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import {NgModule} from '@angular/core';
 import {initializeApp, provideFirebaseApp} from "@angular/fire/app"
 import {getAuth, provideAuth} from "@angular/fire/auth"
 import {FormsModule} from "@angular/forms"
+import {MatButtonModule} from "@angular/material/button"
+import {MatIconModule} from "@angular/material/icon"
+import {MatInputModule} from "@angular/material/input"
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {environment} from "../environments/environment"
@@ -43,6 +46,9 @@ import {JwtInterceptor} from "./services/jwt.interceptor";
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
