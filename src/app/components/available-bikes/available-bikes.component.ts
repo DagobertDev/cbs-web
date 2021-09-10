c0dcb91bf7adc546ee7c7f164bf38438ddb1b69b
David <git@davidmannes.de>
2021-09-10 11:26:26 +0200

    Allow refreshing bikes in overview

--- src/app/components/available-bikes/available-bikes.component.ts
+++ src/app/components/available-bikes/available-bikes.component.ts
@@ -1,6 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {of} from "rxjs"
-import {switchMap} from "rxjs/operators"
 import {Bike} from "../../models/bike"
 import {AuthService} from "../../services/auth.service"
 import {BikeService} from "../../services/bike.service"
@@ -10,12 +9,27 @@ import {BikeService} from "../../services/bike.service"
   templateUrl: './available-bikes.component.html',
   styleUrls: ['./available-bikes.component.css'],
 })
-export class AvailableBikesComponent {
+export class AvailableBikesComponent implements OnInit {
   bikes: Bike[] = []
   newName: string = ""
+  refreshing: boolean = false
 
   constructor(private authService: AuthService, private bikeService: BikeService) {
-    authService.$user.pipe(switchMap(user => user == null ? of([]) : this.bikeService.getByUser(user.id)))
-      .subscribe(bikes => this.bikes = bikes)
+  }
+
+  public ngOnInit() {
+    this.refreshBikes()
+  }
+
+  public refreshBikes() {
+    if (this.refreshing) {
+      return
+    }
+
+    this.refreshing = true
+    this.authService.getUser().then(user => user == null ? of([]) : this.bikeService.getByUser(user.id).subscribe(bikes => {
+      this.bikes = bikes
+      this.refreshing = false
+    }))
   }
 }
