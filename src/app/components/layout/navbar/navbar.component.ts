c105085e47c31a7901f3b1d50330b90326a0b071
David <git@davidmannes.de>
2021-09-10 12:35:33 +0200

    Hide navbar links if not signed in

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {Observable} from "rxjs"
+import {map} from "rxjs/operators"
+import {AuthService} from "../../../services/auth.service"
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +9,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  signedIn: Observable<boolean>
+
+  constructor(private auth: AuthService) {
+    this.signedIn = auth.$user.pipe(map(user => user != null))
+  }
 }
